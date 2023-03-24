8c980ce3bb2bda0c046ac04a5b36647261ccc3c5
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-03-25 00:11:40 +0900

    Initial Commit

---
+++ SampleQiitaList/SampleQiitaListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleQiitaListApp.swift
+//  SampleQiitaList
+//
+//  Created by 渡邊魁優 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleQiitaListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
